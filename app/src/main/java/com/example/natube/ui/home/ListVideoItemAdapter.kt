b88369e208717f2c2fcdb0197d25e94606dd558a
Ohleesang <jammmmed@gmail.com>
2024-02-06 18:40:12 +0900

    feat : Url to Image 를 위한 Glide 종속성 추가

--- app/src/main/java/com/example/natube/ui/home/ListVideoItemAdapter.kt
+++ app/src/main/java/com/example/natube/ui/home/ListVideoItemAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.natube.databinding.VideoItemBinding
 import com.example.natube.model.UnifiedItem
 
@@ -42,6 +43,10 @@ class ListVideoItemAdapter : ListAdapter<UnifiedItem, RecyclerView.ViewHolder>(D
         val item = getItem(position)
         (holder as ListVideoItemViewHolder).apply{
             //이미지
+            Glide.with(itemView)
+                .load(item.thumbnailsUrl)
+                .into(ivVideoItemThumbnail)
+
             //텍스트
             tvVideoItemTitle.text = item.videoTitle
             tvVideoItemChannelName.text = item.channelTitle
