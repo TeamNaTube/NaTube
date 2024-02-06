664bf68c32c74d3b90a37edb5dc5488306e6e142
Ohleesang <jammmmed@gmail.com>
2024-02-06 17:33:13 +0900

    feat : 카테고리 버튼 클릭시 토클 형태로 나타내도록 구현 - Category 에 isClicked 변경을 감지 하여 다시 그리 도록 구현

--- app/src/main/java/com/example/natube/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/natube/ui/home/HomeViewModel.kt
@@ -3,15 +3,24 @@ package com.example.natube.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.hwangtube.network.RetrofitInstance
 import com.example.natube.model.Category
+import com.example.natube.model.UnifiedItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
-    private var _mCategoryList = MutableLiveData<MutableList<Category>>()
-    val mCategoryList: LiveData<MutableList<Category>> get() = _mCategoryList
+    private var _mCategoryList = MutableLiveData<List<Category>>()
+    val mCategoryList: LiveData<List<Category>> get() = _mCategoryList
 
-    init{
+    private var _mUnifiedItemList = MutableLiveData<List<UnifiedItem>>()
+    val mUnifiedItemList: LiveData<List<UnifiedItem>> get() = _mUnifiedItemList
+
+    init {
         initCategoryList()
     }
+
     private fun initCategoryList() {
         val list = mutableListOf<Category>()
         list.add(Category("31", isClicked = true))
@@ -22,12 +31,21 @@ class HomeViewModel : ViewModel() {
         _mCategoryList.value = list
     }
 
-    fun setSelectedItemPosition(position: Int){
-        val newList = mCategoryList.value?.toMutableList()!!
-        for(idx in newList.indices){
+    fun setSelectedItemPosition(position: Int) {
+        val newList = mCategoryList.value!!
+        for (idx in newList.indices) {
             newList[idx].isClicked = idx == position
         }
         _mCategoryList.value = newList
+    }
+
+    fun searchVideoByCategory(categoryId: String) {
+        CoroutineScope(Dispatchers.IO).launch{
+            val items = RetrofitInstance.api.getTrendingVideos().items
+            items.forEach{ item->
+
+            }
+        }
     }
 
 }
