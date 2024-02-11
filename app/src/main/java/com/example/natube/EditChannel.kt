9ce10c104cb7c4b2d2b0cf15fb9ed33886434870
Euny Kim <euny4535@gmail.com>
2024-02-11 13:34:46 +0900

    feat : 정보 수정을 위해 새로운 액티비티 생성

---
+++ app/src/main/java/com/example/natube/EditChannel.kt
@@ -0,0 +1,11 @@
+package com.example.natube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditChannel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_channel)
+    }
+}
