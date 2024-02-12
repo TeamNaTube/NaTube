9e8045d1f6cba6cf316220b791de84e6237c4f77
Ohleesang <jammmmed@gmail.com>
2024-02-13 03:52:32 +0900

    feat : Intro 화면 추가

---
+++ app/src/main/java/com/example/natube/IntroActivity.kt
@@ -0,0 +1,61 @@
+package com.example.natube
+
+import android.animation.Animator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.example.natube.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private val binding: ActivityIntroBinding by lazy { ActivityIntroBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val laLogo = binding.laLogo
+        val laTitle = binding.laTitle
+
+        laTitle.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                val intent = Intent(this@IntroActivity,MainActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+
+        })
+
+        laLogo.apply{
+            setMinAndMaxProgress(0.0f, 0.25f)
+            playAnimation()
+            addAnimatorListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator) {
+                }
+
+                override fun onAnimationEnd(animation: Animator) {
+                    laTitle.apply{
+                        visibility = View.VISIBLE
+                        setMinAndMaxProgress(0.0f, 1.0f)
+                        laTitle.playAnimation()
+                    }
+                }
+
+                override fun onAnimationCancel(animation: Animator) {
+                }
+
+                override fun onAnimationRepeat(animation: Animator) {
+                }
+
+            })
+        }
+    }
+}
